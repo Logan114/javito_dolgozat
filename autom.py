d8fb37111fc4403c453fe18514e6b062df791d0c
logan114 <kepirobalazs204@gmail.com>
2024-01-18 10:38:18 +0100

    Auto vegleg kesz

--- autom.py
+++ autom.py
@@ -4,6 +4,7 @@ import datetime
 file = open("auto.txt","r", encoding="UTF-8")
 file_lista = file.readlines()[1:]
 auto_lista = []
+
 for i in range (0,len(file_lista),1):
     jelen_sor = file_lista[i]
     tisztitott_sor = jelen_sor.strip().split(":")
@@ -20,5 +21,44 @@ def kor(auto_lista,kerdes:str):
         if auto_lista[i].nev == kerdes:
             gyartasiev = int(auto_lista[i].ev)
     kor = year - gyartasiev
-    print (f"{kerdes}, {kor} éves")
+    auto_es_kora = (f"{kerdes} - {kor} éves")
+    print(auto_es_kora)
 
+
+def ki (auto_lista):
+    date = datetime.datetime.now()
+    year = int((date.strftime("%Y")))
+    ki = open ("ki.txt","a", encoding="UTF-8")
+    gy_ev: int = 0
+    auto_nev = ""
+    for i in range (0,len(auto_lista),1):
+        auto_nev = auto_lista[i].nev
+        gy_ev:int = int(auto_lista[i].ev)
+        kor = year - gy_ev
+        ki.write(f"{auto_nev} - {kor} éves\n")
+    ki.close
+
+def flotta(auto_lista):
+    flotta = len(auto_lista)
+    print (f"{flotta} autónk van")
+
+def ertekes(auto_lista):
+    evek_lista = []
+    legoregebb_nev = ""
+    for i in range (0,len(auto_lista),1):
+        evek_lista.append(auto_lista[i].ev)
+    legoregebb = min(evek_lista)
+    for i in range (0,len(auto_lista),1):
+        if auto_lista[i].ev == legoregebb:
+            legoregebb_nev = auto_lista[i].nev
+    print (f"A legöregebb autó: {legoregebb_nev} {legoregebb}")
+
+
+print("I. Kor")
+kor(auto_lista, "Audi A1")
+print("\nII. Flotta")
+flotta(auto_lista)
+print("\nIII. Értékes")
+ertekes(auto_lista)
+print ("IV. Ki")
+ki(auto_lista)
